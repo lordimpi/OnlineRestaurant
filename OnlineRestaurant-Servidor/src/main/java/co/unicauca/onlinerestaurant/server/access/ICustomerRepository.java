13f97e2fa1780e32daabfb16cea6f87308f6a257
Santiago Acuña <snt-26@hotmail.com>
2020-09-21 02:06:49 -0500

    Add template

---
+++ OnlineRestaurant-Servidor/src/main/java/co/unicauca/onlinerestaurant/server/access/ICustomerRepository.java
@@ -0,0 +1,20 @@
+package co.unicauca.onlinerestaurant.server.access;
+
+import co.unicauca.onlinerestaurant.commons.domain.Customer;
+
+/**
+ *
+ * @author Santiago Acuña
+ */
+public interface ICustomerRepository {
+
+    /**
+     * Busca un Customer por su ceduka
+     *
+     * @param id cedula del cliente
+     * @return objeto de tipo Customer
+     */
+    public Customer findCustomer(String id);
+
+    public String createCustomer(Customer customer);
+}
---
+++ OnlineRestaurant-Servidor/src/main/java/co/unicauca/onlinerestaurant/server/app/OnlineRestaurantApplication.java
@@ -0,0 +1,16 @@
+package co.unicauca.onlinerestaurant.server.app;
+
+import co.unicauca.onlinerestaurant.server.infra.OnlineRestaurantServerSocket;
+
+/**
+ * Aplicación principal que lanza el servidor en un hilo
+ *
+ * @author Santiago Acuña
+ */
+public class OnlineRestaurantApplication {
+    public static void main(String[] args) {
+        OnlineRestaurantServerSocket server = new OnlineRestaurantServerSocket();
+        server.start();
+    }
+
+}
