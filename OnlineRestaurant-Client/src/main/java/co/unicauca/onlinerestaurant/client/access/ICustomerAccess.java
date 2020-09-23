75ef5a6e02c1a7988159bf66b75c088632384c6a
Santiago Acuña <snt-26@hotmail.com>
2020-09-23 03:41:53 -0500

    Add the client for the solution

---
+++ OnlineRestaurant-Client/src/main/java/co/unicauca/onlinerestaurant/client/access/ICustomerAccess.java
@@ -0,0 +1,29 @@
+package co.unicauca.onlinerestaurant.client.access;
+
+import co.unicauca.onlinerestaurant.commons.domain.Customer;
+
+/**
+ * Interface que define los servicios de persistencia de Clientes de la agencia
+ *
+ * @author Santiago Acuña
+ */
+public interface ICustomerAccess {
+
+    /**
+     * Buscar un cliente utilizando un socket
+     *
+     * @param id cedula del cliente
+     * @return objeto cliente
+     * @throws Exception error al buscar un cliente
+     */
+    public Customer findCustomer(String id) throws Exception;
+
+    /**
+     * Crea un Customer
+     *
+     * @param customer cliente de la agencia de viajes
+     * @return devuelve la cedula del cliente creado
+     * @throws Exception error crear el cliente
+     */
+    public String createCustomer(Customer customer) throws Exception;
+}
---
+++ OnlineRestaurant-Client/src/main/java/co/unicauca/onlinerestaurant/client/domain/services/UserService.java
@@ -0,0 +1,22 @@
+package co.unicauca.onlinerestaurant.client.domain.services;
+
+import co.unicauca.onlinerestaurant.client.domain.User;
+
+/**
+ * Servicio de usuarios del sistema
+ *
+ * @author Santiago Acuña
+ */
+public class UserService {
+
+    /**
+     * Autentica el usuario en el sistema Por ahora no hace ninguna validación
+     *
+     * @param login login de usuario
+     * @param password contraseña
+     * @return objeto User cuando la autenticación es exitosa
+     */
+    public User autenticacion(String login, String password) {
+        return null;
+    }
+}
