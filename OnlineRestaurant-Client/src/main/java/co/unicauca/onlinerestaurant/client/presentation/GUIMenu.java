43c0d80d3651dae16d09916f3137d1e201aa402c
Santiago Acuña <snt-26@hotmail.com>
2020-09-29 08:53:23 -0500

    Add login template

--- OnlineRestaurant-Client/src/main/java/co/unicauca/onlinerestaurant/client/presentation/GUIMenu.java
+++ OnlineRestaurant-Client/src/main/java/co/unicauca/onlinerestaurant/client/presentation/GUIMenu.java
@@ -9,12 +9,12 @@ package co.unicauca.onlinerestaurant.client.presentation;
  *
  * @author Santiago Acuña
  */
-public class GUIMenu2 extends javax.swing.JFrame {
+public class GUIMenu extends javax.swing.JFrame {
 
     /**
      * Creates new form GUIMenu2
      */
-    public GUIMenu2() {
+    public GUIMenu() {
         initComponents();
     }
 
@@ -83,20 +83,21 @@ public class GUIMenu2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUIMenu2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUIMenu2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUIMenu2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUIMenu2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUIMenu2().setVisible(true);
+                new GUIMenu().setVisible(true);
             }
         });
     }
