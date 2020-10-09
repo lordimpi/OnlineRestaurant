85c2f595db695a5ffe5ad4e32da9160877ef16b2
Santiago Acuña <snt-26@hotmail.com>
2020-10-09 01:01:13 -0500

    Hot fix in modifyDishUI

--- OnlineRestaurant-Client/src/main/java/co/unicauca/onlinerestaurant/client/presentation/GUIModifyDishe.java
+++ OnlineRestaurant-Client/src/main/java/co/unicauca/onlinerestaurant/client/presentation/GUIModifyDishe.java
@@ -8,6 +8,7 @@ package co.unicauca.onlinerestaurant.client.presentation;
 import co.unicauca.onlinerestaurant.client.access.Factory;
 import co.unicauca.onlinerestaurant.client.access.IMainDishAccess;
 import co.unicauca.onlinerestaurant.client.domain.services.MainDishService;
+import co.unicauca.onlinerestaurant.client.infra.Messages;
 import static co.unicauca.onlinerestaurant.client.infra.Messages.successMessage;
 import co.unicauca.onlinerestaurant.commons.domain.MainDish;
 
@@ -22,6 +23,7 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
      */
     public GUIModifyDishe() {
         initComponents();
+        this.jBtnModificar.setVisible(false);
     }
 
     /**
@@ -34,12 +36,16 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jPnNorte = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        jTxfId = new javax.swing.JTextField();
+        jBtnBuscar = new javax.swing.JButton();
         jPnSur = new javax.swing.JPanel();
-        jBtnMidificar = new javax.swing.JButton();
+        jBtnModificar = new javax.swing.JButton();
         jBtnCancelar = new javax.swing.JButton();
         jPnCentro = new javax.swing.JPanel();
-        jTxfId = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
         jTxfNombre = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
         jTxfPrecio = new javax.swing.JTextField();
 
         setClosable(true);
@@ -51,16 +57,22 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
         jPnNorte.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPnNorte.setPreferredSize(new java.awt.Dimension(450, 50));
 
-        javax.swing.GroupLayout jPnNorteLayout = new javax.swing.GroupLayout(jPnNorte);
-        jPnNorte.setLayout(jPnNorteLayout);
-        jPnNorteLayout.setHorizontalGroup(
-            jPnNorteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 491, Short.MAX_VALUE)
-        );
-        jPnNorteLayout.setVerticalGroup(
-            jPnNorteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 48, Short.MAX_VALUE)
-        );
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Id");
+        jPnNorte.add(jLabel4);
+
+        jTxfId.setBackground(new java.awt.Color(255, 255, 255));
+        jTxfId.setForeground(new java.awt.Color(0, 0, 0));
+        jTxfId.setPreferredSize(new java.awt.Dimension(100, 24));
+        jPnNorte.add(jTxfId);
+
+        jBtnBuscar.setText("Buscar");
+        jBtnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBtnBuscarActionPerformed(evt);
+            }
+        });
+        jPnNorte.add(jBtnBuscar);
 
         getContentPane().add(jPnNorte, java.awt.BorderLayout.PAGE_START);
 
@@ -68,13 +80,13 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
         jPnSur.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPnSur.setPreferredSize(new java.awt.Dimension(450, 50));
 
-        jBtnMidificar.setText("Modificar");
-        jBtnMidificar.addActionListener(new java.awt.event.ActionListener() {
+        jBtnModificar.setText("Modificar");
+        jBtnModificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBtnMidificarActionPerformed(evt);
+                jBtnModificarActionPerformed(evt);
             }
         });
-        jPnSur.add(jBtnMidificar);
+        jPnSur.add(jBtnModificar);
 
         jBtnCancelar.setText("Cancelar");
         jBtnCancelar.addActionListener(new java.awt.event.ActionListener() {
@@ -86,15 +98,19 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
 
         getContentPane().add(jPnSur, java.awt.BorderLayout.PAGE_END);
 
-        jPnCentro.setLayout(new java.awt.GridLayout(3, 2));
+        jPnCentro.setLayout(new java.awt.GridLayout(2, 2));
 
-        jTxfId.setBackground(new java.awt.Color(255, 255, 255));
-        jTxfId.setForeground(new java.awt.Color(0, 0, 0));
-        jPnCentro.add(jTxfId);
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Nombre:");
+        jPnCentro.add(jLabel1);
 
         jTxfNombre.setBackground(new java.awt.Color(255, 255, 255));
         jTxfNombre.setForeground(new java.awt.Color(0, 0, 0));
         jPnCentro.add(jTxfNombre);
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Precio:");
+        jPnCentro.add(jLabel2);
 
         jTxfPrecio.setBackground(new java.awt.Color(255, 255, 255));
         jTxfPrecio.setForeground(new java.awt.Color(0, 0, 0));
@@ -110,21 +126,55 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
         this.doDefaultCloseAction();
     }//GEN-LAST:event_jBtnCancelarActionPerformed
 
-    private void jBtnMidificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnMidificarActionPerformed
+    private void jBtnModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnModificarActionPerformed
+
+        String nombre = jTxfNombre.getText();
+        String precio = jTxfPrecio.getText();
+        IMainDishAccess service = Factory.getInstance().getMainDishService();
+        // Inyecta la dependencia
+        MainDishService mainDishService = new MainDishService(service);
+        MainDish dish;
+        if (nombre.equals("") || precio.equals("")) {
+            jTxfNombre.requestFocus();
+            Messages.warningMessage("Campos vacios: Error al modificar", "Warning");
+            return;
+        }
+        try {
+            dish = mainDishService.updateMainDish(this.jTxfId.getText().trim(), this.jTxfNombre.getText(), this.jTxfPrecio.getText());
+        } catch (Exception ex) {
+            clearControls();
+            successMessage(ex.getMessage(), "Atención");
+            return;
+        }
+        clearControls();
+        this.jTxfId.requestFocus();
+        dish = null;
+        successMessage("Se modifico el plato con exito.", "EXITO");
+        this.jBtnModificar.setVisible(false);
+    }//GEN-LAST:event_jBtnModificarActionPerformed
+
+    /**
+     * Metodo encargado de buscar en la base de datos un identificador de un
+     * plato
+     *
+     * @param evt Accion evento del formulario, en este caso accion buscar
+     */
+    private void jBtnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnBuscarActionPerformed
 
         String id = jTxfId.getText().trim();
 
         IMainDishAccess service = Factory.getInstance().getMainDishService();
         // Inyecta la dependencia
         MainDishService mainDishService = new MainDishService(service);
+
         if (id.equals("")) {
             jTxfId.requestFocus();
+            Messages.warningMessage("ERROR: El campo Id esta vacio.", "Warning");
             return;
         }
-
         MainDish dish;
         try {
-            dish = mainDishService.updateMainDish(id, this.jTxfNombre.getText(), this.jTxfPrecio.getText());
+            dish = mainDishService.findMainDish(id);
         } catch (Exception ex) {
             clearControls();
             successMessage(ex.getMessage(), "Atención");
@@ -132,25 +182,38 @@ public class GUIModifyDishe extends javax.swing.JInternalFrame {
         }
         clearControls();
         showData(dish);
-    }//GEN-LAST:event_jBtnMidificarActionPerformed
+        this.jBtnModificar.setVisible(true);
 
+    }//GEN-LAST:event_jBtnBuscarActionPerformed
+
+    /**
+     * Este metodo muestra en el formulario los datos que tiene un objeto
+     *
+     * @param mainDish Objeto plato principal
+     */
     private void showData(MainDish mainDish) {
         jTxfNombre.setText(mainDish.getNameDishe());
         jTxfPrecio.setText(Double.toString(mainDish.getDishPrice()));
 
     }
 
+    /**
+     * Este metodo limpia todos los controles en el formulario
+     */
     public void clearControls() {
 
-        jTxfId.setText("");
         jTxfNombre.setText("");
         jTxfPrecio.setText("");
 
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBtnBuscar;
     private javax.swing.JButton jBtnCancelar;
-    private javax.swing.JButton jBtnMidificar;
+    private javax.swing.JButton jBtnModificar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPnCentro;
     private javax.swing.JPanel jPnNorte;
     private javax.swing.JPanel jPnSur;
